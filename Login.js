08a1f89aa2adc13fac11722e45d2474a6423dc21
sarasaad3885 <collegeapps3885@yahoo.com>
2016-09-26 23:00:43 -0400

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,16 +1,13 @@
 'use strict';
  
-angular.module('myApp.home', ['ngRoute'])
- 
-// Declared route 
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {
-        templateUrl: 'home/home.html',
-        controller: 'HomeCtrl'
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.home'           // Newly added home module
+]).
+config(['$routeProvider', function($routeProvider) {
+    // Set defualt view of our app to home
+     
+    $routeProvider.otherwise({
+        redirectTo: '/home'
     });
-}])
- 
-// Home controller
-.controller('HomeCtrl', [function() {
- 
 }]);
