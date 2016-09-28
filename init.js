2cda0cb19ca88c24a6970d1e9255559fc76f4833
sarasaad3885 <collegeapps3885@yahoo.com>
2016-09-28 19:47:37 -0400

    Firebase Initialization

---
+++ init.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en" ng-app="myApp">
+<head>
+
+<script src="https://www.gstatic.com/firebasejs/3.4.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAXfiPb7lxRd85RpS5WYIIxzwvHvO_ZTOE",
+    authDomain: "temputrip.firebaseapp.com",
+    databaseURL: "https://temputrip.firebaseio.com",
+    storageBucket: "temputrip.appspot.com",
+    messagingSenderId: "423687763023"
+  };
+  firebase.initializeApp(config);
+</script>
+
+</head>
+</html>
