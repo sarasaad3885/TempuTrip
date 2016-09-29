5ef61de907461d560ff3ad2b4ea8166286ec9149
sarasaad3885 <collegeapps3885@yahoo.com>
2016-09-29 10:50:50 -0400

    Update react.js

--- react.js
+++ react.js
@@ -1,10 +1,6 @@
-var Hello = React.createClass({
-  render: function() {
-    return <div>Hello {this.props.name}</div>;
-  }
-});
-
 ReactDOM.render(
-  <Hello name="World" />,
-  document.getElementById('container')
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
 );
+
+<script type="text/babel" src="src/helloworld.js"></script>
