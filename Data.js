c75a71ee92e71ad5e10aa704823ce06994ff94ae
sarasaad3885 <collegeapps3885@yahoo.com>
2016-10-11 14:02:00 -0400

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,10 +1,76 @@
-var items = [
-  { name: 'Washington D.C.', weather: 84, thingstodo: 'museums' },
-  { name: 'Virginia Beach', weather: 78, thingstodo: 'beach'},
-  { name: 'Ocean City', weather:80, thingstodo: 'beach'}
+
+var ServiceChooser = React.createClass({
+
+    getInitialState: function(){
+        return { total: 0 };
+    },
+
+    render: function() {
+
+        var self = this;
+
+        var services = this.props.items.map(function(s){
+
+            // Create a new Service component for each item in the items array.
+            // Notice that I pass the self.addTotal function to the component.
+
+            return <Service name={s.name} price={s.price} active={s.active}  />;
+        });
+
+        return <div>
+                    <h1>Weather Options</h1>
+                    
+                    <div id="services">
+                        {services}
+
+                       
+
+                    </div>
+
+                </div>;
+
+    }
+});
+
+
+var Service = React.createClass({
+
+    getInitialState: function(){
+        return { active: false };
+    },
+
+    clickHandler: function (){
+
+        var active = !this.state.active;
+
+        this.setState({ active: active });
+        
+    
+
+    },
+
+    render: function(){
+
+        return  <p className={ this.state.active ? 'active' : '' } onClick={this.clickHandler}>
+                    {this.props.name} <b>{this.props.price}</b>
+                </p>;
+
+    }
+
+});
+
+
+var services = [
+    { name: 'Washington D.C.', price: 84 },
+    { name: 'Virginia Beach', price: 70 },
+    { name: 'Ocean City', price: 80 },
+    { name: 'Rehoboth Beach', price: 78 }
 ];
 
-React.render(
-  <JsonTable rows={ items } />,
-  document.body
+
+// Render the ServiceChooser component, and pass the array of services
+
+ReactDOM.render(
+    <ServiceChooser items={ services } />,
+    document.getElementById('Select')
 );
