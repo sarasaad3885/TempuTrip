fd2bea3ec6089004e3b299d625ac16c5ea44bbfe
sarasaad3885 <collegeapps3885@yahoo.com>
2016-10-20 13:34:45 -0400

    Update tryJasmine.js

--- tryJasmine.js
+++ tryJasmine.js
@@ -1,50 +1,6 @@
-describe("Investment", function () {
-    var stock, investment;
-
-    beforeEach(function () {
-        stock = {
-            symbol: "AAPL", sharePrice: 4, fetch: function (success) {
-                var _t = this;
-                window.setTimeout(function(){
-                    _t.sharePrice = 23.67;
-                    success.success();
-                },2000);
-            }
-        };
-        investment = {
-            stock: stock,
-            shares: 100,
-            sharePrice: 20
-        };
-    });
-
-    it("should be of a stock", function () {
-        expect(investment.stock).toBe(stock);
-    });
-
-    describe("should be able to update its share price", function () {
-        var fetched = false;
-        beforeEach(function(done){
-            spyOn(stock,"fetch").and.callFake(function(param)
-            {
-                this.sharePrice = 23.67;
-                done();
-            });
-            stock.fetch({
-                success: function () {
-                    fetched = true;
-                    done();
-                }
-            });
-
-        });
-        it("will get the updated price eventually", function(){
-            expect(stock.sharePrice).toEqual(23.67);
-        });
-    });
     describe("render", function(){
     var FooKlass;
-
+    console.log("Hit this line");
     beforeEach(function(){
       FooKlass = React.createClass({
         render: function(){
@@ -62,4 +18,3 @@ describe("Investment", function () {
 
       expect(renderArgs.props.foo).toBe("bar");
     });
-});
