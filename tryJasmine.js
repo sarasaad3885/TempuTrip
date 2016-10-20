ec29df25f9fa0d52bc3bc937688cb44046babdc7
sarasaad3885 <collegeapps3885@yahoo.com>
2016-10-20 12:42:13 -0400

    Update tryJasmine.js

--- tryJasmine.js
+++ tryJasmine.js
@@ -1,3 +1,9 @@
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
 describe('MyComponent', function() {
   var Utils = React.addons.TestUtils;
 
