45a20e0361c66d92e8009f5a98f04ade9890b55c
sarasaad3885 <collegeapps3885@yahoo.com>
2016-10-20 12:42:53 -0400

    Update tryJasmine.js

--- tryJasmine.js
+++ tryJasmine.js
@@ -4,6 +4,16 @@ describe("A suite", function() {
   });
 });
 
+describe("A suite is just a function", function() {
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
+
 describe('MyComponent', function() {
   var Utils = React.addons.TestUtils;
 
