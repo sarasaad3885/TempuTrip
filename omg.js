05b10a494d0a4070bcc54a96de0697a495f0ee52
sarasaad3885 <collegeapps3885@yahoo.com>
2016-10-21 09:55:11 -0400

    Create omg.js

---
+++ omg.js
@@ -0,0 +1,32 @@
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe("A suite is just a function", function() {
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
+
+describe("A spec", function() {
+  it("is just a function, so it can contain any code", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+});
