60c64111815262f9ae85b3e72cb20425bf400552
sarasaad3885 <collegeapps3885@yahoo.com>
2016-10-20 22:24:34 -0400

    Update tryJasmine.js

--- tryJasmine.js
+++ tryJasmine.js
@@ -1,10 +1,10 @@
-describe('TodoApp', function () {
+describe('Home', function () {
         var TestUtils = React.addons.TestUtils;
         var todoAppComponent, element, renderedDOM;
         beforeEach(function (done) {
-            element = React.createElement(TodoApp);
-            todoAppComponent = TestUtils.renderIntoDocument(element);
-            todoAppComponent.setState({items: [{text: "testItem"}]}, done);
+            element = React.createElement(Home);
+            HomeComponent = TestUtils.renderIntoDocument(element);
+            HomeComponent.setState({items: [{text: "testItem"}]}, done);
         });
         it("Has a new button", function () {
             let buttons = TestUtils.scryRenderedDOMComponentsWithTag(todoAppComponent, "button");
@@ -18,43 +18,19 @@ describe('TodoApp', function () {
         });
         it("Has a TodoList component", function () {
             expect(function () {
-                TestUtils.findRenderedComponentWithType(todoAppComponent, TodoList);
+                TestUtils.findRenderedComponentWithType(HomeComponent, TodoList);
             }).not.toThrow();
         });
         describe("New item button", function () {
             beforeEach(function () {
-                spyOn(todoAppComponent.fireRef, "push");
+                spyOn(HomeComponent.fireRef, "push");
             });
             it("Causes fireBase push to be called", function () {
-                let button = TestUtils.scryRenderedDOMComponentsWithTag(todoAppComponent, "button")[1];
+                let button = TestUtils.scryRenderedDOMComponentsWithTag(HomeComponent, "button")[1];
                 TestUtils.Simulate.click(button);
-                expect(todoAppComponent.fireRef.push).toHaveBeenCalledWith({"text": ""});
+                expect(HomeComponent.fireRef.push).toHaveBeenCalledWith({"text": ""});
             });
         });
-        describe("TodoList", function () {
-            var todoListComponent;
-            beforeEach(function(){
-                 todoListComponent = TestUtils.findRenderedComponentWithType(todoAppComponent, TodoList);
-            });
-            it("Updates firebase when text is changed", function(){
-                var setSpy;
-                setSpy = jasmine.createSpy("set");
-                spyOn(todoAppComponent.fireRef, "child").and.returnValue({set : setSpy});
-                var inputs = TestUtils.scryRenderedDOMComponentsWithTag(todoListComponent,"input");
-                inputs[0].value = "asdf";
-                TestUtils.Simulate.change(inputs[0]);
-                expect(todoAppComponent.fireRef.child).toHaveBeenCalled();
-                expect(setSpy).toHaveBeenCalledWith({text: "asdf"});
-            });
-            it("Removes items from firebase when delete is clicked", function(){
-                var deleteSpy;
-                deleteSpy = jasmine.createSpy("remove");
-                spyOn(todoAppComponent.fireRef, "child").and.returnValue({remove : deleteSpy});
-                var deleteButtons = TestUtils.scryRenderedDOMComponentsWithTag(todoListComponent,"button");
-                TestUtils.Simulate.click(deleteButtons[0]);
-                expect(todoAppComponent.fireRef.child).toHaveBeenCalled();
-                expect(deleteSpy).toHaveBeenCalled();
-            });
-        });
+  
 
     });
