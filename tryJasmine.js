c06f90b919268ededbbd0854457e05103fca533b
sarasaad3885 <collegeapps3885@yahoo.com>
2016-10-20 20:41:39 -0400

    Update tryJasmine.js

--- tryJasmine.js
+++ tryJasmine.js
@@ -1,6 +1,8 @@
-    describe("render", function(){
+describe("jasmineReact", function(){
+
+  describe("render", function(){
     var FooKlass;
-    console.log("Hit this line");
+
     beforeEach(function(){
       FooKlass = React.createClass({
         render: function(){
@@ -10,8 +12,15 @@
 
       spyOn(React, "render").andCallThrough();
     });
-    it("should be an object", function () {
-        console.log("inside it()");
+
+    it("should call React.render with the passed in component", function(){
+      jasmineReact.render(<FooKlass foo="bar" />, document.getElementById("Home"));
+
+      var renderArgs = React.render.mostRecentCall.args[0];
+
+      expect(renderArgs.props.foo).toBe("bar");
     });
 
-    });
+ 
+
+});
